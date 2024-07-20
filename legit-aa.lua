ba25ee7dc708e33afc12e685aec30b430a17310c
Ewan Massonet <ewan.massonet@epitech.eu>
2024-07-20 08:57:05 +0200

    [CLN]

--- legit-aa.lua
+++ legit-aa.lua
@@ -1,38 +1,45 @@
 --initialize var
-local ui_refs = {
-	["yaw"] = ui.find("Aimbot", "Anti Aim", "Angles", "Yaw"),
-	["pitch"] = ui.find("Aimbot", "Anti Aim", "Angles", "Pitch"),
+local properties = {
+	ui_refs = {
+		yaw = ui.find("Aimbot", "Anti Aim", "Angles", "Yaw"),
+		pitch = ui.find("Aimbot", "Anti Aim", "Angles", "Pitch")
+	},
+	backup = {
+		yaw = 0,
+		pitch = 0
+	},
+	run = false
 }
-local backup = {
-	["yaw"] = ui_refs.yaw:get(),
-	["pitch"] = ui_refs.pitch:get(),
-}
-local running = 0
+properties.backup.yaw = properties.ui_refs.yaw:get()
+properties.backup.pitch = properties.ui_refs.pitch:get()
+
+--initialize menu
+local group = ui.create("Legit Anti-Aim")
+local enable_on_freestanding = group:switch("Enable on freestanding", false)
 
 --functions
 local function legit_aa(cmd)
-	ui_refs.yaw:set("Disabled")
-	ui_refs.pitch:set("Disabled")
+	properties.ui_refs.yaw:set("Disabled")
+	properties.ui_refs.pitch:set("Disabled")
 end
 
 --register events
 events.createmove:set(function(cmd)
 	if cmd.in_use then
-		if running == 0 then
-			backup.yaw = ui_refs.yaw:get()
-			backup.pitch = ui_refs.pitch:get()
-			running = 1
+		if not properties.run then
+			properties.backup.yaw = properties.ui_refs.yaw:get()
+			properties.backup.pitch = properties.ui_refs.pitch:get()
+			properties.run = true
 		end
 	else
-		if running == 1 then
-			ui_refs.yaw:set(backup.yaw)
-			ui_refs.pitch:set(backup.pitch)
-			running = 0
+		if properties.run then
+			properties.ui_refs.yaw:set(properties.backup.yaw)
+			properties.ui_refs.pitch:set(properties.backup.pitch)
+			properties.run = false
 		end
-
 	end
 	
-	if running == 1 then
+	if properties.run then
 		cmd.in_use = false
 		legit_aa(cmd)
 	end
