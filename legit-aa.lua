c99c2907a33881df3a25e606da1e7b4617390743
Ewan Massonet <ewan.massonet@epitech.eu>
2024-07-20 09:01:08 +0200

    []CLN] || [ADD] on freestand

--- legit-aa.lua
+++ legit-aa.lua
@@ -2,13 +2,15 @@
 local properties = {
 	ui_refs = {
 		yaw = ui.find("Aimbot", "Anti Aim", "Angles", "Yaw"),
-		pitch = ui.find("Aimbot", "Anti Aim", "Angles", "Pitch")
+		pitch = ui.find("Aimbot", "Anti Aim", "Angles", "Pitch"),
+		freestand = ui.find("Aimbot", "Anti Aim", "Angles", "Freestanding")
 	},
 	backup = {
 		yaw = 0,
 		pitch = 0
 	},
-	run = false
+	run = false,
+	enable_on_freestanding = true
 }
 properties.backup.yaw = properties.ui_refs.yaw:get()
 properties.backup.pitch = properties.ui_refs.pitch:get()
@@ -18,14 +20,8 @@ local group = ui.create("Legit Anti-Aim")
 local enable_on_freestanding = group:switch("Enable on freestanding", false)
 
 --functions
-local function legit_aa(cmd)
-	properties.ui_refs.yaw:set("Disabled")
-	properties.ui_refs.pitch:set("Disabled")
-end
-
---register events
-events.createmove:set(function(cmd)
-	if cmd.in_use then
+local function update_state(cmd)
+	if cmd.in_use or (properties.enable_on_freestanding and properties.ui_refs.freestand:get()) then
 		if not properties.run then
 			properties.backup.yaw = properties.ui_refs.yaw:get()
 			properties.backup.pitch = properties.ui_refs.pitch:get()
@@ -38,7 +34,16 @@ events.createmove:set(function(cmd)
 			properties.run = false
 		end
 	end
-	
+end
+
+local function legit_aa(cmd)
+	properties.ui_refs.yaw:set("Disabled")
+	properties.ui_refs.pitch:set("Disabled")
+end
+
+--register events
+events.createmove:set(function(cmd)
+	update_state(cmd)
 	if properties.run then
 		cmd.in_use = false
 		legit_aa(cmd)
